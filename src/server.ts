d9b7b2bb27544bbde8bb25421be87e32e83fe85c
Gregory Jean-Baptiste <gregory.from.miami@gmail.com>
2021-01-24 12:31:47 -0800

    adds tooling for further development

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, { NextFunction } from 'express';
+import bodyParser from 'body-parser';
+
+const loggerMiddleware: express.RequestHandler = (request: express.Request, response: express.Response, next: NextFunction) => {
+  console.log(`${request.method} ${request.path}`);
+  next();
+}
  
-const app = express();
+const app: express.Application = express();
+app.use(bodyParser);
+app.use(loggerMiddleware);
  
 app.get('/', (request: express.Request, response: express.Response) => {
   response.send('Hello world!');
+});
+
+app.post('/', (request: express.Request, response: express.Response) => {
+  response.send(request.body);
 });
  
 app.listen(5000);
